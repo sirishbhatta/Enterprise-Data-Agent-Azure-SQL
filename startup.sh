fe74fad55581923db1982baa6a75287acf146a2a
Sirish Bhatta <sirishbhatta@gmail.com>
2026-04-06 14:37:14 -0500

    changed the startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # DO NOT put pip install here - Azure already did it!
-python -m streamlit run app.py --server.port 8080 --server.address 0.0.0.0 --server.headless true
+python -m streamlit run app.py --server.port=8080 --server.address=0.0.0.0 --server.headless=true
